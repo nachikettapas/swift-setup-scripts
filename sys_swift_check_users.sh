865f13fcbdc381a8e66e83f1da0fe24d387f8770
Nandini Tata <nandini.tata@intel.com>
2016-09-29 16:33:55 +0000

    added a check for sudoers

--- sys_swift_check_users.sh
+++ sys_swift_check_users.sh
@@ -22,14 +22,14 @@
 # Script to check if "swift" exists; if not, will be created
 ############################################################
 
-SWIFT_USER="swift"
-SWIFT_GROUP="swift"
-
 # Ensures the script is being run as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+SWIFT_USER="swift"
+SWIFT_GROUP="swift"
 
 #verify that swift group exists
 if grep -q ${SWIFT_GROUP} /etc/group; then
@@ -50,6 +50,7 @@ else
   adduser ${SWIFT_USER} sudo
   echo "swift user has been added to the  group"
   echo "try 'sudo su swift' to switch to swift user and it will not prompt for password"
+fi
    
 #set no password for swift. 
 if grep -q ${SWIFT_GROUP} /etc/sudoers; then
